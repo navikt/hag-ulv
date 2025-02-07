15a734c04f682987a408879c11a1564ec4904aee
Jesper Hustad <jesper.forrest.hustad@nav.no>
2025-02-07 16:45:13 +0100

    Slettet uviktige filer

--- kataloger/dialogporten/test.js
+++
@@ -1,5 +0,0 @@
-const getMaskinportenToken = require("../old-getMaskinportenToken.js");
-
-console.error("From the outside!");
-
-getMaskinportenToken("maskinporten-hag-lps-api-client-").then((result) => {console.log(result)});
