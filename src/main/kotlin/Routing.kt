a32be5c92087abe02fefee6ac144bb5bfffa605f
Jesper Hustad <jesper.forrest.hustad@nav.no>
2025-02-26 10:27:13 +0100

    Fikset bruno til å lese lokal fil

--- src/main/kotlin/Routing.kt
+++ src/main/kotlin/Routing.kt
@@ -7,46 +7,17 @@ import io.ktor.server.http.content.*
 import io.ktor.server.plugins.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
+import io.ktor.util.pipeline.*
 import java.io.File
 
 fun Application.configureRouting() {
     routing {
         get("/token/{service-navn}/{ekstra-scope?}") {
-            try {
-                val serviceNavn = call.parameters["service-navn"] ?: throw BadRequestException("Mangler parameter Maskinporten service")
-
-                val ekstraScope = call.parameters["ekstra-scope"]
-
-                val token = getMaskinportenToken(serviceNavn, ekstraScope)
-
-                call.respondText(token)
-            } catch (e: SocketTimeoutException) {
-                call.respond(
-                    HttpStatusCode.Unauthorized,
-                    "Timeout med KubeCTL. Dette kan forekomme om du gjør for mange JWK secret spørringer innen kort tid\nError message: ${e.message}",
-                )
-            } catch (e: Exception) {
-                call.respond(HttpStatusCode.BadRequest, "Error: ${e.message}")
-            }
+            handleGetTokenResponse()
         }
 
         get("/altinn-token/{service-navn}/{ekstra-scope?}") {
-            try {
-                val serviceNavn = call.parameters["service-navn"] ?: throw BadRequestException("Mangler parameter Maskinporten service")
-
-                val ekstraScope = call.parameters["ekstra-scope"]
-
-                val token = getAltinnToken(serviceNavn, ekstraScope)
-
-                call.respondText(token)
-            } catch (e: SocketTimeoutException) {
-                call.respond(
-                    HttpStatusCode.Unauthorized,
-                    "Timeout med KubeCTL. Dette kan forekomme om du gjør for mange JWK secret spørringer innen kort tid\nError message: ${e.message}",
-                )
-            } catch (e: Exception) {
-                call.respond(HttpStatusCode.BadRequest, "Error: ${e.message}")
-            }
+            handleGetTokenResponse(veksleMedAltinn = true)
         }
 
         // Swagger nettside
@@ -64,5 +35,27 @@ fun Application.configureRouting() {
                 }
             }
         }
+    }
+}
+
+private suspend fun PipelineContext<Unit, ApplicationCall>.handleGetTokenResponse(veksleMedAltinn: Boolean = false) {
+    try {
+        val serviceNavn =
+            call.parameters["service-navn"] ?: throw BadRequestException("Mangler parameter Maskinporten service")
+
+        val ekstraScope = call.parameters["ekstra-scope"]
+
+        val maskinportenToken = getMaskinportenToken(serviceNavn, ekstraScope)
+
+        val token = if (veksleMedAltinn) maskinportenToken.veksleTilAltinnToken() else maskinportenToken
+
+        call.respondText(token)
+    } catch (e: SocketTimeoutException) {
+        call.respond(
+            HttpStatusCode.Unauthorized,
+            "Timeout med KubeCTL. Dette kan forekomme om du gjør for mange JWK secret spørringer innen kort tid\nError message: ${e.message}",
+        )
+    } catch (e: Exception) {
+        call.respond(HttpStatusCode.BadRequest, "Error: ${e.message}")
     }
 }
